7f25bd8184545d83f4f1fb22aecc603b19d0a7ad
Witizli <benaiah.finkel@heig-vd.ch>
2022-10-03 15:24:05 +0200

    creation base de donnée et création des différentes chemins

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-import createError from "http-errors"
+import createError from "http-errors";
 import express from "express";
 import logger from "morgan";
+
+// Indique nos différentes routes URL
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
+import productsRouter from "./routes/products.js";
+import messagesRouter from "./routes/messages.js";
+import reviewsRouter from "./routes/reviews.js";
+
+//Permet de se connecter a la base de donée
+import mongoose from "mongoose";
+mongoose.Promise = Promise;
+// ou est stocker la base de donée
+mongoose.connect("mongodb://localhost/tesseve");
 
 const app = express();
 
@@ -10,8 +21,12 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Indique nos différentes routes URL
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/messages", messagesRouter);
+app.use("/reviews", reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/message.js
+++ models/message.js
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 // Define the schema for users
 const messageSchema = new Schema({
-    content: String
+    content: String,
 });
 // Create the model from the schema and export it
-export default mongoose.model('Message', messageSchema);
+export default mongoose.model("Message", messageSchema);
