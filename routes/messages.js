56e3d0ff92003a709e272794bd41c6c567204ece
Vincent <vincent.tarrit29@gmail.com>
2022-10-03 16:02:17 +0200

    delete route messages useless

--- routes/messages.js
+++ routes/messages.js
@@ -2,10 +2,6 @@ import express from "express";
 import { index, store } from "../controllers/MessageController.js";
 const router = express.Router();
 
-router.get("/", function(req, res, next) {
-    res.send("Got a response from the messages route");
-});
-
 router.get("/", index);
 
 router.post("/", store);
