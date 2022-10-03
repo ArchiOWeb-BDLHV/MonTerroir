4272724b340eff460505b7af08d0f760b4142d27
Dario Antonini <dario.antonini@heig-vd.ch>
2022-10-03 15:55:02 +0200

    Messages update

--- routes/messages.js
+++ routes/messages.js
@@ -1,8 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
     res.send("Got a response from the messages route");
 });
+
+router.get("/", index);
+
+router.post("/", store);
 
 export default router;
