c24fefa9fe6c1ed645ac8ee2206d09dc8d5060ab
Vincent <vincent.tarrit29@gmail.com>
2022-10-10 11:02:25 +0200

    default port config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
     db: {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
