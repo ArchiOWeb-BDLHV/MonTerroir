20c9a0a4eb8efe871d0026e3470da59ae3b9a8a4
Dario Antonini <dario.antonini@heig-vd.ch>
2022-10-10 11:59:41 +0200

    update message.js

--- app/models/message.js
+++ app/models/message.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
+
 // Define the schema for users
 const messageSchema = new Schema({
-    content: String,
+    name: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return v.length > 1 && v.length <= 400;
+            },
+            message: props => `${props.value} is not a valid message! Message must be between 1 and 400 characters.`
+        },
+    },
 });
+
 // Create the model from the schema and export it
 export default mongoose.model("Message", messageSchema);
