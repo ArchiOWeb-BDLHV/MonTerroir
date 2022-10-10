076ab4dce237a8461538d71ae853f3fe22228c0c
Witizli <benaiah.finkel@heig-vd.ch>
2022-10-10 13:42:19 +0200

    ok

--- app/models/user.js
+++ app/models/user.js
@@ -1,21 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function(v) {
-                return v.length > 2 && v.length <= 70;
-            },
-            message: props => `${props.value} is not a valid name! Name must be between 3 and 70 characters.`
-        },
+  name: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v.length > 2 && v.length <= 70;
+      },
+      message: (props) =>
+        `${props.value} is not a valid name! Name must be between 3 and 70 characters.`,
     },
+  },
 });
 
-userSchema.statics.findByName = function(name) {
-    return this.find({ name: new RegExp(name, 'i') });
+userSchema.statics.findByName = function (name) {
+  return this.find({ name: new RegExp(name, "i") });
 };
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
