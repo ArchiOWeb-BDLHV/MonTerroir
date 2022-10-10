6d9608febd19e64529b7c11c725dab6abb96a3dd
laurehel <laureh.rev@gmail.com>
2022-10-10 12:07:30 +0200

    Controller, model & route Review modified

--- app/http/controllers/ReviewController.js
+++ app/http/controllers/ReviewController.js
@@ -1,53 +1,74 @@
 import Review from "../../models/review.js";
 
-function index(req, res, next) {
-    Review.find().exec(function(err, reviews) {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).json(reviews);
-    });
+async function index(req, res, next) {
+    const reviewsScore = await Review.find().sort('score');
+    res.status(200).json(reviewsScore);
+    const reviewsMsg = await Review.find().sort('message');
+    res.status(200).json(reviewsMsg);
+
+    //// Ancienne version, sans safeRoute et sans async/await
+    // Review.find().exec(function(err, reviews) {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     res.status(200).json(reviews);
+    // });
 }
 
-function store(req, res, next) {
+async function store(req, res, next) {
     const review = new Review({
         score: req.body.score,
         message: req.body.message
     });
-    review.save(function(err) {
-        if (err) {
-            return next(err);
-        }
-        res.status(201).send(review);
-    });
+    const result = await review.save();
+    res.status(201).json(result);
+
+    //// Ancienne version, sans safeRoute et sans async/await
+    // review.save(function(err) {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     res.status(201).send(review);
+    // });
 }
 
-function show(req, res, next) {
-    Review.findById(req.params.id).exec(function(err, reviews) {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).json(reviews);
-    });
+async function show(req, res, next) {
+    const review = await Review.findById(req.params.id);
+    res.status(200).json(review);
+
+    //// Ancienne version, sans safeRoute et sans async/await
+    // Review.findById(req.params.id).exec(function(err, reviews) {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     res.status(200).json(reviews);
+    // });
 }
 
-function update(req, res, next) {
-    Review.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, function(err, review) {
-        if (err) {
-            return next(err);
-        }
-        res.status(201).json(review);
-    });
+async function update(req, res, next) {
+    const review = await Review.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+    res.status(200).json(review);
+
+    //// Ancienne version, sans safeRoute et sans async/await
+    // Review.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, function(err, review) {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     res.status(201).json(review);
+    // });
 }
 
-function destroy(req, res, next) {
-    Review.findOneAndDelete({ _id: req.params.id }, function(err, review) {
-        if (err) {
-            return next(err);
-        }
-        res.status(204).json();
-    });
-}
+async function destroy(req, res, next) {
+    const review = await Review.findOneAndDelete({ _id: req.params.id });
+    res.status(204).json();
 
+    //// Ancienne version, sans safeRoute et sans async/await
+    // Review.findOneAndDelete({ _id: req.params.id }, function(err, review) {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     res.status(204).json();
+    // });
+}
 
 export { index, store, show, update, destroy };
--- app/models/review.js
+++ app/models/review.js
@@ -1,10 +1,26 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
+
 // Define the schema for reviews
 const reviewSchema = new Schema({
-    score: Number,
-    message: String
+    score: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+    },
+    message: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return v.length > 0 && v.length <= 1000;
+            },
+            message: props => `${props.value} is not a valid message! Message must be between 1 and 1000 characters.`
+        },
+    },
 });
+
 // Create the model from the schema and export it
 export default mongoose.model('Review', reviewSchema);
