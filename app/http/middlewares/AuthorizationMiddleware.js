a03ab15feabe4120732cc2a93df1e918d901b47b
Vincent <vincent.tarrit29@gmail.com>
2022-10-24 14:31:22 +0200

    fix typo

--- app/http/middlewares/AuthorizationMiddleware.js
+++ app/http/middlewares/AuthorizationMiddleware.js
@@ -1,13 +1,11 @@
 export function is(role) {
-    return [
-        (req, res, next) => {
-            const hasRight = (req.user.role & role) ? true : false;
-            if (!hasRight) { // On vérifie si le role de l'utilisateur correspond au role passé en paramètre
-                return res.status(403).json({
-                    message: 'You don\'t have the right to access this resource.',
-                });
-            }
-            next();
+    return (req, res, next) => {
+        const hasRight = (req.user.role & role) ? true : false;
+        if (!hasRight) { // On vérifie si le role de l'utilisateur correspond au role passé en paramètre
+            return res.status(403).json({
+                message: 'You don\'t have the right to access this resource.',
+            });
         }
-    ];
+        next();
+    };
 }
