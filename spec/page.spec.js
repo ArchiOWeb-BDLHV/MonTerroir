52f5895062f49ec497948bd8b4bf651ce9b1d276
Vincent <vincent.tarrit29@gmail.com>
2022-11-19 16:57:16 +0100

    fix test

--- spec/page.spec.js
+++ spec/page.spec.js
@@ -6,7 +6,7 @@ describe('pages', function() {
     it("should redirect page 404 to the documentation", async function() {
         const res = await supertest(app)
             .get('/404')
-            .expect(302);
+            .expect(404);
     });
 });
 
