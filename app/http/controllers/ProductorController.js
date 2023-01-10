c7c1e0a9b423efff155aa3af1bccc27fe8e01a33
Vincent <vincent.tarrit29@gmail.com>
2023-01-10 16:48:06 +0100

    ok

--- app/http/controllers/ProductorController.js
+++ app/http/controllers/ProductorController.js
@@ -40,7 +40,7 @@ export class ProductorController {
         try {
             //hide properties from response
             const productorsFiltered = productors.map(productor => {
-                let obj = productor.toJSON();
+                let obj = productor;
                 delete obj.password;
                 delete obj.__v;
                 delete obj.role;
