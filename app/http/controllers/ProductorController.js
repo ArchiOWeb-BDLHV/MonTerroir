378ad5659b59c20b43550e402a6fc4bf06937696
Vincent <vincent.tarrit29@gmail.com>
2023-01-10 16:59:07 +0100

    ok

--- app/http/controllers/ProductorController.js
+++ app/http/controllers/ProductorController.js
@@ -49,12 +49,9 @@ export class ProductorController {
                 delete obj.createdAt;
                 delete obj.type;
 
-                /* obj.images = obj.images.map(image => {
-                    let objI = image;
-                    delete objI.__v;
-                    objI.url = config.appUrl + objI.url;
-                    return objI;
-                }); */
+                obj.images = obj.images.map(image => {
+                    return image.toJSON();
+                });
                 return obj;
             });
             res.status(200).json(productorsFiltered);
