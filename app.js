30ce39acf2ba598e2129399e6d3940b304dbe730
Vincent <vincent.tarrit29@gmail.com>
2023-01-16 15:09:26 +0100

    ok

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cors())
 
 
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 
 //check if user is authenticated
 
