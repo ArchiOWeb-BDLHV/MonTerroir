effc92fd05307d210658805f48c68ce0e9efedf8
Vincent <vincent.tarrit29@gmail.com>
2023-01-16 15:02:42 +0100

    body size

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.bodyParser({ limit: '50mb' }));
 
 //check if user is authenticated
 
