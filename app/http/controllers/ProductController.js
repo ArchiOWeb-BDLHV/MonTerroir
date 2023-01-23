9365c9f82c55857c6bf9393b8991772515174ef6
vincent-tarrit <vincent.tarrit29@gmail.com>
2023-01-23 11:07:42 +0100

    test productor

--- app/http/controllers/ProductController.js
+++ app/http/controllers/ProductController.js
@@ -11,129 +11,158 @@ import { randomUUID } from "crypto";
 const debug = createDebugger("express-api:product");
 
 export class ProductController {
-    static async index(req, res, next) {
-        let products;
-        if (req.query.category) {
-            //where products categories includes req.query.category with query
-            products = await Product.find({ categories: req.query.category }).sort("name").populate(["images", "categories"])
-        } else {
-            products = await Product.find().sort("name").populate(["images", "categories"])
-        }
-        res.status(200).json(products);
+  static async index(req, res, next) {
+    let products;
+    if (req.query.category) {
+      //where products categories includes req.query.category with query
+      products = await Product.find({ categories: req.query.category })
+        .sort("name")
+        .populate(["images", "categories"]);
+    } else {
+      products = await Product.find()
+        .sort("name")
+        .populate(["images", "categories"]);
     }
 
-    static async store(req, res, next) {
-        let images = [];
-        if (req.body.images) {
-            for (const image of req.body.images) {
-                // deplacer image sur serveur
-                const path = "/uploads/" + Date.now() + "_" + Math.floor(Math.random() * 100000) + ".png";
-                const url = process.cwd() + "/public" + path;
-
-                //create folder if not exist
-                if (!fs.existsSync(process.cwd() + "/public/uploads")) {
-                    fs.mkdirSync(process.cwd() + "/public/uploads", { recursive: true });
-                }
-
-                let base64Image = image.split(';base64,').pop();
-                fs.writeFileSync('image.png', base64Image, {encoding: 'base64'});
-                fs.renameSync(process.cwd() + "/image.png", url);
-
-                const i = await Image.create({
-                    url: config.appUrl + path,
-                });
-                images.push(i);
-            }
-        }
-        try {
-            const categories = await Category.find({ _id: { $in: req.body.categories } });
-
-            const product = new Product({
-                name: req.body.name,
-                description: req.body.description,
-                price: req.body.price,
-                categories: categories,
-                images: images,
-            });
-
-            const result = await product.save();
-
-            //attach product to user 
-            const user = req.user;
-            user.products.push(result);
-            await user.save();
-            res.status(201).json(result);
-        } catch (e) {
-            if (e.name === "ValidationError") {
-                nonProcessable(next, e);
-            }
-        }
+    //find productor who has this product
+    for (const product of products) {
+      const user = await User.findOne({ products: product._id });
+      product.productor = user;
     }
+    res.status(200).json(products);
+  }
 
-    static async show(req, res, next) {
-        const product = await Product.findById(req.params.id).populate("images").populate("categories");
-        if (!product) {
-            return res.status(404).json({ message: "Product not found" });
-        }
-        res.status(200).json(product);
-    }
+  static async store(req, res, next) {
+    let images = [];
+    if (req.body.images) {
+      for (const image of req.body.images) {
+        // deplacer image sur serveur
+        const path =
+          "/uploads/" +
+          Date.now() +
+          "_" +
+          Math.floor(Math.random() * 100000) +
+          ".png";
+        const url = process.cwd() + "/public" + path;
 
-    static async update(req, res, next) {
-        let images = [];
-        if (req.body.images) {
-            for (const image of req.body.images) {
-                // deplacer image sur serveur
-                const path = "/uploads/" + Date.now() + "_" + Math.floor(Math.random() * 100000) + ".png";
-                const url = process.cwd() + "/public" + path;
-
-                //create folder if not exist
-                if (!fs.existsSync(process.cwd() + "/public/uploads")) {
-                    fs.mkdirSync(process.cwd() + "/public/uploads", { recursive: true });
-                }
-
-                let base64Image = image.split(';base64,').pop();
-                fs.writeFileSync('image.png', base64Image, {encoding: 'base64'});
-                fs.renameSync(process.cwd() + "/image.png", url);
-
-                const i = await Image.create({
-                    url: config.appUrl + path,
-                });
-                images.push(i);
-            }
+        //create folder if not exist
+        if (!fs.existsSync(process.cwd() + "/public/uploads")) {
+          fs.mkdirSync(process.cwd() + "/public/uploads", { recursive: true });
         }
 
-        const product = await Product.findOneAndUpdate({ _id: req.params.id }, {
-            name: req.body.name,
-            description: req.body.description,
-            price: req.body.price,
-            categories: [...req.body.categories],
-            images: [...images],
+        let base64Image = image.split(";base64,").pop();
+        fs.writeFileSync("image.png", base64Image, { encoding: "base64" });
+        fs.renameSync(process.cwd() + "/image.png", url);
 
-        }, {
-            new: true,
+        const i = await Image.create({
+          url: config.appUrl + path,
         });
-        res.status(200).json(product);
+        images.push(i);
+      }
     }
+    try {
+      const categories = await Category.find({
+        _id: { $in: req.body.categories },
+      });
 
-    static async destroy(req, res, next) {
-        const product = await Product.findOneAndDelete({ _id: req.params.id });
-        res.status(204).json();
+      const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        categories: categories,
+        images: images,
+      });
+
+      const result = await product.save();
+
+      //attach product to user
+      const user = req.user;
+      user.products.push(result);
+      await user.save();
+      res.status(201).json(result);
+    } catch (e) {
+      if (e.name === "ValidationError") {
+        nonProcessable(next, e);
+      }
     }
+  }
 
-    static async mine(req, res, next) {
-        const user = await User.findById(req.user._id).populate({
-            path: "products",
-            populate: {
-                path: "images",
-                model: "Image",
-            },
-        }).populate({
-            path: "products",
-            populate: {
-                path: "categories",
-                model: "Category",
-            },
+  static async show(req, res, next) {
+    const product = await Product.findById(req.params.id)
+      .populate("images")
+      .populate("categories");
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  }
+
+  static async update(req, res, next) {
+    let images = [];
+    if (req.body.images) {
+      for (const image of req.body.images) {
+        // deplacer image sur serveur
+        const path =
+          "/uploads/" +
+          Date.now() +
+          "_" +
+          Math.floor(Math.random() * 100000) +
+          ".png";
+        const url = process.cwd() + "/public" + path;
+
+        //create folder if not exist
+        if (!fs.existsSync(process.cwd() + "/public/uploads")) {
+          fs.mkdirSync(process.cwd() + "/public/uploads", { recursive: true });
+        }
+
+        let base64Image = image.split(";base64,").pop();
+        fs.writeFileSync("image.png", base64Image, { encoding: "base64" });
+        fs.renameSync(process.cwd() + "/image.png", url);
+
+        const i = await Image.create({
+          url: config.appUrl + path,
         });
-        res.status(200).json(user.products);
+        images.push(i);
+      }
     }
+
+    const product = await Product.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        categories: [...req.body.categories],
+        images: [...images],
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(product);
+  }
+
+  static async destroy(req, res, next) {
+    const product = await Product.findOneAndDelete({ _id: req.params.id });
+    res.status(204).json();
+  }
+
+  static async mine(req, res, next) {
+    const user = await User.findById(req.user._id)
+      .populate({
+        path: "products",
+        populate: {
+          path: "images",
+          model: "Image",
+        },
+      })
+      .populate({
+        path: "products",
+        populate: {
+          path: "categories",
+          model: "Category",
+        },
+      });
+    res.status(200).json(user.products);
+  }
 }
