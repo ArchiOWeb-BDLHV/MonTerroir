99fbb4fbe12ec6142150723436d0fcfa077e12eb
vincent-tarrit <vincent.tarrit29@gmail.com>
2023-01-23 10:31:53 +0100

    add image to categories

--- app/models/category.js
+++ app/models/category.js
@@ -2,37 +2,44 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function(v) {
-                return v.length > 2 && v.length <= 70;
-            },
-            message: (props) =>
-                `${props.value} is not a valid name! Name must be between 3 and 70 characters.`,
-        },
+  name: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v.length > 2 && v.length <= 70;
+      },
+      message: (props) =>
+        `${props.value} is not a valid name! Name must be between 3 and 70 characters.`,
     },
-    description: {
-        type: String,
-        required: false,
-        default: ""
+  },
+  description: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
     },
-    products: [{
-        type: Schema.Types.ObjectId,
-        ref: "Product",
-    }, ],
+  ],
+
+  image: {
+    type: String,
+    required: false,
+    default: "",
+  },
 });
 
-categorySchema.statics.findByName = function(name) {
-    return this.find({ name: new RegExp(name, "i") });
+categorySchema.statics.findByName = function (name) {
+  return this.find({ name: new RegExp(name, "i") });
 };
 
-categorySchema.statics.createFake = async function() {
-    return await this.create({
-        name: "Fake category",
-    });
-}
-
+categorySchema.statics.createFake = async function () {
+  return await this.create({
+    name: "Fake category",
+  });
+};
 
 export default mongoose.model("Category", categorySchema);
