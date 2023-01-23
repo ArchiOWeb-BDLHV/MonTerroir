a4ee65a479587ef78b65a82a23a0e8576d54c88f
vincent-tarrit <vincent.tarrit29@gmail.com>
2023-01-23 10:33:45 +0100

    store image category

--- app/http/controllers/CategoryController.js
+++ app/http/controllers/CategoryController.js
@@ -1,38 +1,41 @@
 import Category from "../../models/category.js";
 
 export class CategoryController {
-    static async index(req, res, next) {
-        const categories = await Category.find().sort("name");
-        res.status(200).json(categories);
-    }
+  static async index(req, res, next) {
+    const categories = await Category.find().sort("name");
+    res.status(200).json(categories);
+  }
 
-    static async store(req, res, next) {
-        const category = await Category.create({
-            name: req.body.name,
-            description: req.body.description,
-        });
-        res.status(201).json(category);
-    }
+  static async store(req, res, next) {
+    const category = await Category.create({
+      name: req.body.name,
+      description: req.body.description,
+      image: req.body.image,
+    });
+    res.status(201).json(category);
+  }
 
-    static async show(req, res, next) {
-        const category = await Category.findById(req.params.id);
-        if (!category) {
-            return res.status(404).json({ message: "Category not found" });
-        }
-        res.status(200).json(category);
+  static async show(req, res, next) {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
     }
+    res.status(200).json(category);
+  }
 
-    static async update(req, res, next) {
-        const category = await Category.findOneAndUpdate({ _id: req.params.id },
-            req.body, {
-                new: true,
-            }
-        );
-        res.status(200).json(category);
-    }
+  static async update(req, res, next) {
+    const category = await Category.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(category);
+  }
 
-    static async destroy(req, res, next) {
-        const category = await Category.findOneAndDelete({ _id: req.params.id });
-        res.status(204).json();
-    }
+  static async destroy(req, res, next) {
+    const category = await Category.findOneAndDelete({ _id: req.params.id });
+    res.status(204).json();
+  }
 }
